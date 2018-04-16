5e158923a4fea331e9cf97319f0cdb2d9dbd87b4
root <root@ub16-base>
2018-04-16 16:01:37 +0300

    Test Alpha - 1.1

--- demo-Java-Eclipse-ConnectMySQL-servlet/src/demo/java/eclipse/connectMySQL/servlet/ConnectMySQLServlet.java
+++ demo-Java-Eclipse-ConnectMySQL-servlet/src/demo/java/eclipse/connectMySQL/servlet/ConnectMySQLServlet.java
@@ -1,36 +1,25 @@
+// MySQL Servlet request demo
+
 package demo.java.eclipse.connectMySQL.servlet;
 
 import java.io.*;
-
 import java.sql.*;
 import javax.sql.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ConnectMySQLServlet
- */
 @WebServlet("/ConnectMySQLServlet")
 public class ConnectMySQLServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public ConnectMySQLServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+        super();        
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		System.out.println("Test Connection to MySQL");
@@ -83,11 +72,7 @@ public class ConnectMySQLServlet extends HttpServlet {
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
